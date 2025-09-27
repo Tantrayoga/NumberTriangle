2159a4c1c1600ddf869b43d2df30b1a465d10c85
Tantrayoga <arieltantra1@gmail.com>
2025-09-27 00:46:45 -0400

    Implement Retrieve. Fixes #2

--- src/main/java/NumberTriangle.java
+++ src/main/java/NumberTriangle.java
@@ -88,8 +88,19 @@ public class NumberTriangle {
      *
      */
     public int retrieve(String path) {
-        // TODO implement this method
-        return -1;
+        NumberTriangle curr = this;
+        if (path == null || path.isEmpty()) {
+            return curr.root;
+        }
+        for (int i = 0; i < path.length(); i++) {
+            char ch = path.charAt(i);
+            if (ch == 'l') {
+                curr = curr.left;
+            } else if (ch == 'r') {
+                curr = curr.right;
+            }
+        }
+        return curr.root;
     }
 
     /** Read in the NumberTriangle structure from a file.
@@ -113,7 +124,6 @@ public class NumberTriangle {
         BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
 
 
-        // TODO define any variables that you want to use to store things
         NumberTriangle[] prevRow = null;
 
         // will need to return the top of the NumberTriangle,
